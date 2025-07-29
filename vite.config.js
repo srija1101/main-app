46f03850822ec91e14ad76f52cd43895385cf3f6
Srija <2320030093@klh.edu.in>
2025-07-29 10:14:24 +0530

    fix: fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
   plugins: [
     react(),
     federation({
-      name: 'main_app',
       remotes: {
-        music_library: 'music_library@https://music-library-67ld7mi48-srijas-projects-f5af826b.vercel.app/remoteEntry.js'
+        music_library: 'music_library@https://music-library-omvusitfn-srijas-projects-f5af826b.vercel.app/remoteEntry.js'
       },
       shared: ['react', 'react-dom']
     })
